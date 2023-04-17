9b361a4099c16fca6b755d8e9663852525b6fde9
Sangsan <sangsan.prohmvitak@gmail.com>
2023-04-18 02:31:48 +0700

    Modularize test_accounting.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from financial.accounting import Category, Account, print_balance_sheet, print_composite
-from models import SqliteDb
+from tests.test_accounting import initialize_SqliteDb
 
 if __name__ == "__main__":
-    # TODO make test file for this
-    sqlite_db = SqliteDb('ledger.db')  # initialize SqliteDb
-    # category_dict = sqlite_db.read_categories(name='Assets')
-    # category_dict = sqlite_db.read_categories(name='Liabilities')
-    # print(category_dict)
-    # print_composite(category_dict)
-
-    # TODO calculate_every_category() is ok but how to test for upsert category, upsert account, and read_category.
-    # make test for that
+    sqlite_db, assets, liabilities = initialize_SqliteDb()
+    print(sqlite_db, assets, liabilities)
+    # TODO make print_balance_sheet() work for sql data
 
     # TODO CLI input form for category, then for account
     # TODO provide "disable" bool to both category and account records. Edit all queries or db-read opr accordingly.
